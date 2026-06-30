f42eacd460d1e849df71af92d55b9b672e8f6a64
Praffultiwari56 <121789613+Praffultiwari56@users.noreply.github.com>
2026-06-30 14:49:43 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://hhyvzsrbrjhyfovljfxg.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_fHBGhjgN__JZjIny23yw7g_p863UNk8";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
